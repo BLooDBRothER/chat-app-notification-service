522dc670d8fa11a53cfeb50010a6664e6cd8424b
Arul Murugavel <63643748+BLooDBRothER@users.noreply.github.com>
2024-07-25 18:17:27 +0530

    Initialize firebase

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
-import initAllConsumers from "./consumer";
+import initializeFirebase from "@/config/firebase";
+import initAllConsumers from "@/consumer";
 
 import { config } from "dotenv";
 
 config();
+
+initializeFirebase();
 
 initAllConsumers();
